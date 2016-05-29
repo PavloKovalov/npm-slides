6e0aac4d60878bf38898970f16df76af2b44811d
Maxim Schepelin <m.schepelin@gmail.com>
2016-05-29 20:08:55 +0300

    add package.json example

--- presentation/index.js
+++ presentation/index.js
@@ -38,7 +38,10 @@ const theme = createTheme({
   textPrimary: "#f6cc3d",
 })
 
-const code = {}
+const code = {
+  npmInit: require('raw!../assets/npm_init.example'),
+  'package.json': require('raw!../package.json'),
+}
 
 export default class Presentation extends React.Component {
   render() {
@@ -54,10 +57,62 @@ export default class Presentation extends React.Component {
             </Heading>
           </Slide>
           <Slide>
+            <List>
             <Text textColor="textSecondary" bold margin="0 0 15px">What is npm?</Text>
             <Text textColor="textSecondary" bold margin="0 0 15px">What is package.json file?</Text>
             <Text textColor="textSecondary" bold margin="0 0 15px">How to manage dependencies?</Text>
+            <Text textColor="textSecondary" bold margin="0 0 15px">What is CommonJS?</Text>
+            <Text textColor="textSecondary" bold margin="0 0 15px">ES2015 modules</Text>
+            </List>
           </Slide>
+          <Slide>
+            <Heading size={4} textColor="textPrimary">
+              npm
+            </Heading>
+            <List>
+              <Text textColor="textSecondary" margin="0 0 15px">CLI for dependencies managmenet</Text>
+              <Text textColor="textSecondary" margin="0 0 15px">Entry point to the application</Text>
+              <Text textColor="textSecondary" margin="0 0 15px">Allows run scripts</Text>
+              <Text textColor="textSecondary" margin="0 0 15px">New dependencies can be saved to package.json</Text>
+            </List>
+          </Slide>
+
+          <CodeSlide
+            transition={["slide"]}
+            lang="js"
+            code={code.npmInit}
+            ranges={[
+              { loc: [0, 0], title: "Init new project" },
+              { loc: [0, 1], note: "Run `npm init` from console" },
+              { loc: [1, 9], note: "Command info" },
+              { loc: [10, 20], note: "Fill up requred fields" },
+              { loc: [22, 33], note: "Preview the result" },
+              { loc: [35, 36], note: "Write `package.json` file" },
+            ]}
+          />
+
+          <CodeSlide
+            transition={["slide"]}
+            lang="js"
+            code={code['package.json']}
+            ranges={[
+              { loc: [0, 0], title: "package example" },
+              { loc: [1, 4], note: "package name, version, description" },
+              { loc: [4, 5], note: "Entry point" },
+              { loc: [5, 13], note: "Custom scripts executed by `npm run <script>`" },
+              { loc: [10, 12], note: "Command and hook" },
+              { loc: [15, 25], note: "Project dependencies" },
+              { loc: [16, 18], note: "^ means update major versions automatically" },
+              { loc: [21, 22], note: "~ means update minor versions automatically" },
+              { loc: [26, 32], note: "Development environment dependencies" },
+              { loc: [26, 26], note: "New dependecy will be added if `npm install <name>` called with --save or --save-dev" },
+            ]}
+          />
+        <Slide>
+          <Heading size={2} textColor="textPrimary">
+            Before ES2015
+          </Heading>
+        </Slide>
 
         </Deck>
       </Spectacle>
